8617cdb4f889f0628868d066dc8dec6e64d59a2b
Jacob <Jacobabrooks04@gmail.com>
2023-06-29 15:05:53 +0100

    weewsdewr

--- Backend/database/setup.sql
+++ Backend/database/setup.sql
@@ -13,3 +13,16 @@ CREATE TABLE show(
 
 INSERT INTO show (name, duration, genre) 
 VALUES ('Matilda The Musical', 155, 'Musical' ), ('Wicked',165,'Musical')
+
+CREATE TABLE theatre(
+    theatre_id  INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL, 
+    location NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (theatre_id)
+);
+
+INSERT INTO show (name, duration, genre) 
+VALUES ('Matilda The Musical', 155, 'Musical' ), ('Wicked',165,'Musical')
+
--- Backend/routes/showRoute.js
+++ Backend/routes/showRoute.js
@@ -6,5 +6,7 @@ router.get('/',ShowController.getAllShows)
 router.post('/', ShowController.createShow)
 router.get('/:id',ShowController.getShowById)
 router.put('/:id', ShowController.updateShow)
+router.delete('/:id', ShowController.deleteShow)
+
 
 module.exports = router
